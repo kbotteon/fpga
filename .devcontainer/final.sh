1998f28d0e9271a4b7046b08852261769d42980c
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2023-09-30 18:03:21 +0000

    chore: clean up Dockerfile and final.sh

--- .devcontainer/final.sh
+++ .devcontainer/final.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+mkdir -p /workspaces/tmp
+mkdir -p /workspaces/tmp/dot-ssh
+mkdir -p /workspaces/tools
+
 ln -s ${PWD}/.devcontainer/persist ${HOME}/persist
+ln -s /workspaces/tmp/dot-ssh ${HOME}/.ssh
