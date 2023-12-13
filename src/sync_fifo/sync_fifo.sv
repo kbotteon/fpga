e3ec3df0b598b635fe87f6b4199105277a4a3353
Kyle Botteon <49845890+kbotteon@users.noreply.github.com>
2023-12-12 21:47:37 -0800

    fix: drop gt, lt comparisons from sync_fifo

--- src/sync_fifo/sync_fifo.sv
+++ src/sync_fifo/sync_fifo.sv
@@ -2,7 +2,7 @@
 * \file
 * \brief A synchronous FIFO
 *******************************************************************************/
-`timescale 1ns/1ns
+`timescale C_TIMESCALE
 
 module sync_fifo #(
     parameter WIDTH = 16,
@@ -13,113 +13,121 @@ module sync_fifo #(
     input logic i_rst,
     // Write Bus
     input logic [WIDTH-1:0] i_data,
-    input logic i_wren,
+    input logic i_wr_en,
     output logic o_full,
-    output logic o_werr,
+    output logic o_wr_err,
     // Read Bus
-    input logic i_rden,
+    input logic i_rd_incr,
     output logic [WIDTH-1:0] o_data,
     output logic o_empty,
-    output logic o_rerr
+    output logic o_rd_err
 );
 
 localparam ADDR_WIDTH = $clog2(DEPTH)-1;
 
-initial assert(WIDTH*DEPTH <= 1024)
-    else $error("That's a pretty large LUTRAM");
-
-//------------------------------------------------------------------------------
-// Memories
-//------------------------------------------------------------------------------
-
-// Distributed RAM repurposes LUTs as RAM, which is more area efficient than
-// using the limited flops in a CLB; use BRAM or true dual-port (async) FIFO
-(* RAM_STYLE="distributed") logic [WIDTH-1:0] ram [0:DEPTH-1];
-
-// If it's a Xilinx device, initialize the RAM in the bitfile
-// FIXME: This works for BRAM, but does it also work for LUTRAM?
-generate if(TARGET == "Xilinx") begin
-    initial for(int addr = 0; addr < DEPTH; addr = addr+1) begin
-        ram[addr] <= 0;
-    end
-end
+initial assert(WIDTH <= 256*8196)
+    else $error("That's a massive FIFO");
 
 //------------------------------------------------------------------------------
 // Nets
 //------------------------------------------------------------------------------
 
-logic [ADDR_WIDTH:0] read_addr = 0;
-logic [ADDR_WIDTH:0] write_addr = 0;
-// The actual occupancy, not occupancy-1
-logic [DEPTH:0] occupancy = 0;
+logic [ADDR_WIDTH-1:0] read_addr = 0;
+logic [ADDR_WIDTH-1:0] write_addr = 0;
+logic [ADDR_WIDTH:0] occupancy = 0;
 
-logic read_allowed = 0;
-logic write_allowed = 0;
+logic do_read;
+logic read_allowed;
+logic do_write;
+logic write_allowed;
+
+//------------------------------------------------------------------------------
+// Memories
+//------------------------------------------------------------------------------
+
+// In Xilinx devices, distributed RAM repurposes CLBs into a RAM with density
+// between using CLB flops and BRAM
+(* RAM_STYLE="DISTRIBUTED" *) logic [WIDTH-1:0] ram [0:DEPTH-1];
+
+// Xilinx devices can initialize CLBs and BRAMs using the bitstream
+generate
+    if(TARGET == "Xilinx") begin
+        initial for(int addr = 0; addr < DEPTH; addr = addr + 1) begin
+            ram[addr] = 0;
+        end
+    end
+endgenerate
+
+// Write access
+always_ff@(posedge i_clk) begin
+    if(i_wr_en) begin
+        ram[write_addr] <= i_data;
+    end
+end
+
+// Read access
+always_ff@(posedge i_clk) begin
+    if(i_rst) begin
+        o_data <= '0;
+    end else begin
+        o_data <= ram[read_addr]
+    end
+end
 
 //------------------------------------------------------------------------------
 // Logic
 //------------------------------------------------------------------------------
 
-//
-// Access permissions
-//
-
-always_comb begin
-    write_allowed = occupancy < DEPTH;
-    read_allowed = occupancy > 0;
+always_comb begin : access_triggers
+    do_read == i_rd_incr && read_allowed;
+    do_write = i_wr_en && write_allowed;
 end
 
-//
-// Occupancy
-// We might want this as an output at some point anyway, and it'll help timing
-always_ff@(posedge i_clk) begin
+always_comb begin : access_permissions
+    write_allowed = occupancy != DEPTH;
+    read_allowed = occupancy != 0;
+end
+
+always_ff@(posedge i_clk) begin : occupancy_counter
     if(i_rst) begin
         occupancy <= '0;
     end else begin
-        if(write_allowed && i_wren) begin
+        if(do_read && !do_write) begin
             occupancy <= occupancy + 1;
-        end else if(read_allowed && i_rden) begin
+        end else if(do_write && !do_read) begin
             occupancy <= occupancy - 1;
+        end else begin // if((do_write && do_read) || (!do_write && !do-read))
+            occupancy <= occupancy;
         end
     end
 end
 
-
-//
-// Read and Write Pointers
-//
-
-// TODO: These should never pass one another
-always_ff@(posedge i_clk) begin
+always_ff@(posedge i_clk) begin : access_pointers
     if(i_rst) begin
-        read_addr <= 0;
-        write_addr <= 0;
+        read_addr <= '0;
+        write_addr <= '0;
     end else begin
-        // Read increments as current value is being read, when allowed
-        if(i_rden && read_allowed) begin
-            read_addr <= read_addr + 1;
+        // Read pointer
+        if(do_read) begin
+            // Roll over the counter
+            if(read_addr == DEPTH-1) begin
+                read_addr <= '0;
+            end else begin
+                read_addr <= read_addr + 1;
+            end
         end
-        // Write increments as current value is being written, when allowed
-        if(i_wren && write_allowed) begin
-            write_addr <= write_addr + 1;
+        // Write pointer
+        if(do_write) begin
+            // Roll over the counter
+            if(write_addr == DEPTH-1) begin
+                write_addr <= '0;
+            end else begin
+                write_addr <= write_addr + 1;
+            end
         end
     end
 end
 
-//
-// Ports
-//
-
-always_ff@(posedge i_clk) begin
-    // FIXME: Can we reset LUTRAM?
-
-    // Read port
-    o_data <= ram[read_addr];
-
-    // Write port
-    if(i_wren) begin
-        ram[write_addr] <= i_data;
-    end
-end
+//------------------------------------------------------------------------------
 
 endmodule
